51b33daec1dac846154825c84ad83e3e3f5f4d73
ChengQing <darkblue035@gmail.com>
2022-01-23 21:13:17 +0800

    added voice pronunciation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use colored::*;
-use regex::Regex;
-use reqwest::get;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 use std::env::args;
-use urlencoding::encode;
+use std::error::Error;
+use wd_dict::Lookup;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let mut rl = Editor::<()>::new();
     let history_path = format!(
         "{}/wd-history.txt",
@@ -15,14 +13,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
     rl.load_history(&history_path).ok();
     if args().len() <= 1 {
+        let mut lookup = Lookup::new();
         loop {
             let readline = rl.readline("~ ");
             match readline {
-                Ok(line) => {
-                    rl.add_history_entry(line.as_str());
-                    search(line.as_str()).await?;
-                    println!();
-                }
+                Ok(line) => match &line[..] {
+                    "" => continue,
+                    "1" => {
+                        lookup.play_uk_pronunciation().await;
+                        continue;
+                    }
+                    "2" => {
+                        lookup.play_us_pronunciation().await;
+                        continue;
+                    }
+                    _ => {
+                        rl.add_history_entry(line.as_str());
+                        lookup = Lookup::search(line.as_str()).await;
+                        println!();
+                    }
+                },
                 Err(ReadlineError::Interrupted) => break, // ctrl + c
                 Err(ReadlineError::Eof) => break,         // ctrl + d
                 Err(err) => {
@@ -35,89 +45,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let keywords: Vec<String> = args().collect();
         let keywords = &keywords[1..].join(" ");
         rl.add_history_entry(keywords);
-        search(keywords).await?
+        Lookup::search(keywords).await;
     }
+
     rl.save_history(&history_path).unwrap();
 
     Ok(())
-}
-
-async fn search(text: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let url = String::from("https://dict.youdao.com/w/") + &encode(text);
-
-    let res = get(&url).await?;
-    let body = res.text().await?;
-    print_pronunciation(&body);
-    print_meaning(&body);
-
-    Ok(())
-}
-
-fn print_pronunciation(text: &str) {
-    let reg = Regex::new(r#"<span class="phonetic">[\s\S]*?</span>"#).unwrap();
-    let mut results = vec![];
-
-    for item in reg.captures_iter(text) {
-        results.push(
-            item.get(0)
-                .unwrap()
-                .as_str()
-                .replace(r#"<span class="phonetic">"#, "")
-                .replace("</span>", ""),
-        );
-    }
-
-    if results.len() == 2 {
-        let to_print = format!("英 {}  美 {}", results[0], results[1]);
-        println!("{}", to_print.cyan());
-    }
-}
-fn print_meaning(text: &str) {
-    let is_translate = Regex::new(r#"<div id="fanyiToggle">"#)
-        .unwrap()
-        .is_match(text);
-    if is_translate {
-        let meaning_paragraph = Regex::new(r#"<div class="trans-container">[\s\S]*?</div>"#)
-            .unwrap()
-            .find(text)
-            .map(|x| x.as_str())
-            .unwrap();
-        let mut results = vec![];
-        let reg = Regex::new(r#"<p>[\s\S]*?</p>"#).unwrap();
-        for item in reg.captures_iter(meaning_paragraph) {
-            results.push(
-                item.get(0)
-                    .unwrap()
-                    .as_str()
-                    .replace("<p>", "")
-                    .replace("</p>", ""),
-            );
-        }
-        println!("{}", results[1]);
-    } else {
-        let is_found = Regex::new(r#"<h2 class="wordbook-js">"#)
-            .unwrap()
-            .is_match(text);
-        if is_found {
-            let meaning_paragraph = Regex::new(r#"<ul>[\s\S]*?</ul>"#)
-                .unwrap()
-                .find(text)
-                .map(|x| x.as_str())
-                .unwrap();
-            let mut results = vec![];
-            let reg = Regex::new(r#"<li>[\s\S]*?</li>"#).unwrap();
-            for item in reg.captures_iter(meaning_paragraph) {
-                results.push(
-                    item.get(0)
-                        .unwrap()
-                        .as_str()
-                        .replace("<li>", "")
-                        .replace("</li>", ""),
-                );
-            }
-            println!("{}", results.join("\n"));
-        } else {
-            println!("{}", "No result".red());
-        }
-    }
 }
