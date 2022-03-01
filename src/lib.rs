3d5b3c1dba0b636d84aece72bb47b31153db75b8
ChengQing <darkblue035@gmail.com>
2022-03-02 00:56:19 +0800

    update rodio to 0.15.0

--- src/lib.rs
+++ src/lib.rs
@@ -3,287 +3,287 @@ use reqwest;
 
 #[derive(Debug)]
 pub enum Status {
-    Success,
-    NoResult,
+  Success,
+  NoResult,
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Suggestion {
-    pub word: String,
-    pub definition: String,
+  pub word: String,
+  pub definition: String,
 }
 
 #[derive(Debug, PartialEq)]
 pub struct LookupResult {
-    pub uk_pronunciation: Option<String>,
-    pub us_pronunciation: Option<String>,
-    pub definition: Option<String>,
-    pub suggestions: Option<Vec<Suggestion>>,
+  pub uk_pronunciation: Option<String>,
+  pub us_pronunciation: Option<String>,
+  pub definition: Option<String>,
+  pub suggestions: Option<Vec<Suggestion>>,
 }
 
 pub async fn lookup(to_search: &str) -> std::result::Result<LookupResult, reqwest::Error> {
-    let url = format!("https://dict.youdao.com/w/{}", to_search);
-    let response = reqwest::get(url).await?.text().await?;
+  let url = format!("https://dict.youdao.com/w/{}", to_search);
+  let response = reqwest::get(url).await?.text().await?;
 
-    let is_translate = Regex::new(r#"<div id="fanyiToggle">"#)
-        .unwrap()
-        .is_match(&response);
+  let is_translate = Regex::new(r#"<div id="fanyiToggle">"#)
+    .unwrap()
+    .is_match(&response);
 
-    if is_translate {
-        let definition = grab_translation(&response);
-        return Ok(LookupResult {
-            uk_pronunciation: None,
-            us_pronunciation: None,
-            definition: definition,
-            suggestions: None,
-        });
-    }
+  if is_translate {
+    let definition = grab_translation(&response);
+    return Ok(LookupResult {
+      uk_pronunciation: None,
+      us_pronunciation: None,
+      definition: definition,
+      suggestions: None,
+    });
+  }
 
-    let is_chinese = Regex::new("英语怎么说").unwrap().is_match(&response);
+  let is_chinese = Regex::new("英语怎么说").unwrap().is_match(&response);
 
-    if is_chinese {
-        let definition = grab_english_words(&response);
-        return Ok(LookupResult {
-            uk_pronunciation: None,
-            us_pronunciation: None,
-            definition: definition,
-            suggestions: None,
-        });
-    }
+  if is_chinese {
+    let definition = grab_english_words(&response);
+    return Ok(LookupResult {
+      uk_pronunciation: None,
+      us_pronunciation: None,
+      definition: definition,
+      suggestions: None,
+    });
+  }
 
-    let has_suggestion = Regex::new("您要找的是不是").unwrap().is_match(&response);
-    let has_result = Regex::new(r#"<h2 class="wordbook-js">"#)
-        .unwrap()
-        .is_match(&response);
+  let has_suggestion = Regex::new("您要找的是不是").unwrap().is_match(&response);
+  let has_result = Regex::new(r#"<h2 class="wordbook-js">"#)
+    .unwrap()
+    .is_match(&response);
 
-    let suggestions = if has_suggestion {
-        grab_suggestion(&response)
-    } else {
-        None
-    };
-    let definition = if has_result {
-        grab_formal_definition(&response)
-    } else {
-        None
-    };
-    let uk_pronunciation = grab_uk_pronunciation(&response);
-    let us_pronunciation = grab_us_pronunciation(&response);
+  let suggestions = if has_suggestion {
+    grab_suggestion(&response)
+  } else {
+    None
+  };
+  let definition = if has_result {
+    grab_formal_definition(&response)
+  } else {
+    None
+  };
+  let uk_pronunciation = grab_uk_pronunciation(&response);
+  let us_pronunciation = grab_us_pronunciation(&response);
 
-    Ok(LookupResult {
-        uk_pronunciation,
-        us_pronunciation,
-        definition,
-        suggestions,
-    })
+  Ok(LookupResult {
+    uk_pronunciation,
+    us_pronunciation,
+    definition,
+    suggestions,
+  })
 }
 
 fn grab_translation(response: &str) -> Option<String> {
-    let definition_part = Regex::new(r#"(<div class="trans-container">)[\s\S]*?(</div>)"#)
-        .unwrap()
-        .find(response)
-        .map(|x| x.as_str());
+  let definition_part = Regex::new(r#"(<div class="trans-container">)[\s\S]*?(</div>)"#)
+    .unwrap()
+    .find(response)
+    .map(|x| x.as_str());
 
-    match definition_part {
-        None => return None,
-        Some(definition_part) => {
-            let definition = Regex::new(r#"<p>[\s\S]*?</p>"#)
-                .unwrap()
-                .find_iter(&definition_part)
-                .map(|x| x.as_str())
-                .collect::<Vec<&str>>()[1]
-                .replace("<p>", "")
-                .replace("</p>", "");
-            return Some(definition);
-        }
+  match definition_part {
+    None => return None,
+    Some(definition_part) => {
+      let definition = Regex::new(r#"<p>[\s\S]*?</p>"#)
+        .unwrap()
+        .find_iter(&definition_part)
+        .map(|x| x.as_str())
+        .collect::<Vec<&str>>()[1]
+        .replace("<p>", "")
+        .replace("</p>", "");
+      return Some(definition);
     }
+  }
 }
 
 fn grab_english_words(response: &str) -> Option<String> {
-    let definition_part = Regex::new(r#"(<div class="trans-container">)[\s\S]*?(</div>)"#)
+  let definition_part = Regex::new(r#"(<div class="trans-container">)[\s\S]*?(</div>)"#)
+    .unwrap()
+    .find(response)
+    .map(|x| x.as_str());
+
+  match definition_part {
+    None => return None,
+    Some(definition_part) => {
+      let definition_raw = Regex::new(r#"(<p class="wordGroup">)[\s\S]*?(</p>)"#)
         .unwrap()
-        .find(response)
-        .map(|x| x.as_str());
-
-    match definition_part {
-        None => return None,
-        Some(definition_part) => {
-            let definition_raw = Regex::new(r#"(<p class="wordGroup">)[\s\S]*?(</p>)"#)
-                .unwrap()
-                .find_iter(&definition_part)
-                .map(|x| {
-                    x.as_str()
-                        .replace(r#"<p class="wordGroup">"#, "")
-                        .replace("</p>", "")
-                })
-                .collect::<Vec<String>>();
-
-            let mut definition_lines = vec![];
-            for item in definition_raw {
-                let word_type = Regex::new(r#"(;">)[\s\S]*?(</span>)"#)
-                    .unwrap()
-                    .find(&item)
-                    .map(|x| x.as_str().replace(r#";">"#, "").replace("</span>", ""));
-                let definition = Regex::new(r#"(E2Ctranslation">)[\s\S]*?(</a>)"#)
-                    .unwrap()
-                    .find(&item)
-                    .map(|x| {
-                        x.as_str()
-                            .replace(r#"E2Ctranslation">"#, "")
-                            .replace("</a>", "")
-                    });
-
-                let mut definition_line = String::new();
-                if let Some(word_type) = word_type {
-                    if !word_type.contains(";") {
-                        definition_line.push_str(&word_type);
-                        definition_line.push_str(" ");
-                    }
-                }
-                if let Some(definition) = definition {
-                    definition_line.push_str(&definition);
-                }
-                if definition_line.len() > 0 {
-                    definition_lines.push(definition_line);
-                }
-            }
-            if definition_lines.len() > 0 {
-                return Some(definition_lines.join("/n"));
-            } else {
-                return None;
-            }
-        }
-    }
-}
-
-fn grab_formal_definition(response: &str) -> Option<String> {
-    let definition_part = Regex::new(r#"(<div class="trans-container">\s*<ul>)[\s\S]*?(</ul>)"#)
-        .unwrap()
-        .find(response)
-        .map(|x| x.as_str());
-
-    if definition_part == None {
-        return None;
-    }
-
-    let definition = Regex::new(r#"(<li>)[\s\S]*?(</li>)"#)
-        .unwrap()
-        .find_iter(&definition_part.unwrap())
-        .map(|x| x.as_str().replace("<li>", "").replace("</li>", ""))
-        .collect::<Vec<String>>()
-        .join("\n");
-
-    Some(definition)
-}
-
-fn grab_uk_pronunciation(response: &str) -> Option<String> {
-    Regex::new(r#"(<span class="pronounce">英[\s\S]*<span class="phonetic">)[\s\S]*?(</span>)"#)
-        .unwrap()
-        .find(response)
+        .find_iter(&definition_part)
         .map(|x| {
-            Regex::new(r#"<span class="pronounce">英[\s\S]*<span class="phonetic">|</span>"#)
-                .unwrap()
-                .replace_all(x.as_str(), "")
-                .into_owned()
-        })
-}
-fn grab_us_pronunciation(response: &str) -> Option<String> {
-    Regex::new(r#"(<span class="pronounce">美[\s\S]*<span class="phonetic">)[\s\S]*?(</span>)"#)
-        .unwrap()
-        .find(response)
-        .map(|x| {
-            Regex::new(r#"<span class="pronounce">美[\s\S]*<span class="phonetic">|</span>"#)
-                .unwrap()
-                .replace_all(x.as_str(), "")
-                .into_owned()
-        })
-}
-
-fn grab_suggestion(response: &str) -> Option<Vec<Suggestion>> {
-    let suggestion_raw = Regex::new(r#"(<p class="typo-rel">)[\s\S]*?(</p>)"#)
-        .unwrap()
-        .find_iter(response)
-        .map(|x| {
-            x.as_str()
-                .replace(r#"<p class="typo-rel">"#, "")
-                .replace("</p>", "")
+          x.as_str()
+            .replace(r#"<p class="wordGroup">"#, "")
+            .replace("</p>", "")
         })
         .collect::<Vec<String>>();
 
-    if suggestion_raw.len() == 0 {
-        return None;
-    }
+      let mut definition_lines = vec![];
+      for item in definition_raw {
+        let word_type = Regex::new(r#"(;">)[\s\S]*?(</span>)"#)
+          .unwrap()
+          .find(&item)
+          .map(|x| x.as_str().replace(r#";">"#, "").replace("</span>", ""));
+        let definition = Regex::new(r#"(E2Ctranslation">)[\s\S]*?(</a>)"#)
+          .unwrap()
+          .find(&item)
+          .map(|x| {
+            x.as_str()
+              .replace(r#"E2Ctranslation">"#, "")
+              .replace("</a>", "")
+          });
 
-    let mut suggestions = vec![];
-
-    for item in suggestion_raw {
-        let word = Regex::new(r#"(class="search-js">)[\s\S]*?(</a></span>)"#)
-            .unwrap()
-            .find(&item)
-            .map(|x| {
-                x.as_str()
-                    .replace(r#"class="search-js">"#, "")
-                    .replace("</a></span>", "")
-            });
-        let definition = Regex::new(r#"(</span>)[\s\S]*"#)
-            .unwrap()
-            .find(&item)
-            .map(|x| x.as_str().replace(r#"</span>"#, ""))
-            .unwrap_or_else(|| String::from(""))
-            .trim()
-            .to_owned();
-        if let Some(word) = word {
-            suggestions.push(Suggestion {
-                word: word,
-                definition,
-            })
+        let mut definition_line = String::new();
+        if let Some(word_type) = word_type {
+          if !word_type.contains(";") {
+            definition_line.push_str(&word_type);
+            definition_line.push_str(" ");
+          }
         }
-    }
-
-    if suggestions.len() == 0 {
+        if let Some(definition) = definition {
+          definition_line.push_str(&definition);
+        }
+        if definition_line.len() > 0 {
+          definition_lines.push(definition_line);
+        }
+      }
+      if definition_lines.len() > 0 {
+        return Some(definition_lines.join("/n"));
+      } else {
         return None;
+      }
     }
+  }
+}
 
-    Some(suggestions)
+fn grab_formal_definition(response: &str) -> Option<String> {
+  let definition_part = Regex::new(r#"(<div class="trans-container">\s*<ul>)[\s\S]*?(</ul>)"#)
+    .unwrap()
+    .find(response)
+    .map(|x| x.as_str());
+
+  if definition_part == None {
+    return None;
+  }
+
+  let definition = Regex::new(r#"(<li>)[\s\S]*?(</li>)"#)
+    .unwrap()
+    .find_iter(&definition_part.unwrap())
+    .map(|x| x.as_str().replace("<li>", "").replace("</li>", ""))
+    .collect::<Vec<String>>()
+    .join("\n");
+
+  Some(definition)
+}
+
+fn grab_uk_pronunciation(response: &str) -> Option<String> {
+  Regex::new(r#"(<span class="pronounce">英[\s\S]*<span class="phonetic">)[\s\S]*?(</span>)"#)
+    .unwrap()
+    .find(response)
+    .map(|x| {
+      Regex::new(r#"<span class="pronounce">英[\s\S]*<span class="phonetic">|</span>"#)
+        .unwrap()
+        .replace_all(x.as_str(), "")
+        .into_owned()
+    })
+}
+fn grab_us_pronunciation(response: &str) -> Option<String> {
+  Regex::new(r#"(<span class="pronounce">美[\s\S]*<span class="phonetic">)[\s\S]*?(</span>)"#)
+    .unwrap()
+    .find(response)
+    .map(|x| {
+      Regex::new(r#"<span class="pronounce">美[\s\S]*<span class="phonetic">|</span>"#)
+        .unwrap()
+        .replace_all(x.as_str(), "")
+        .into_owned()
+    })
+}
+
+fn grab_suggestion(response: &str) -> Option<Vec<Suggestion>> {
+  let suggestion_raw = Regex::new(r#"(<p class="typo-rel">)[\s\S]*?(</p>)"#)
+    .unwrap()
+    .find_iter(response)
+    .map(|x| {
+      x.as_str()
+        .replace(r#"<p class="typo-rel">"#, "")
+        .replace("</p>", "")
+    })
+    .collect::<Vec<String>>();
+
+  if suggestion_raw.len() == 0 {
+    return None;
+  }
+
+  let mut suggestions = vec![];
+
+  for item in suggestion_raw {
+    let word = Regex::new(r#"(class="search-js">)[\s\S]*?(</a></span>)"#)
+      .unwrap()
+      .find(&item)
+      .map(|x| {
+        x.as_str()
+          .replace(r#"class="search-js">"#, "")
+          .replace("</a></span>", "")
+      });
+    let definition = Regex::new(r#"(</span>)[\s\S]*"#)
+      .unwrap()
+      .find(&item)
+      .map(|x| x.as_str().replace(r#"</span>"#, ""))
+      .unwrap_or_else(|| String::from(""))
+      .trim()
+      .to_owned();
+    if let Some(word) = word {
+      suggestions.push(Suggestion {
+        word: word,
+        definition,
+      })
+    }
+  }
+
+  if suggestions.len() == 0 {
+    return None;
+  }
+
+  Some(suggestions)
 }
 
 pub struct Voice {
-    pub uk: Option<String>,
-    pub us: Option<String>,
+  pub uk: Option<String>,
+  pub us: Option<String>,
 }
 
 impl Voice {
-    pub fn new() -> Self {
-        Self { uk: None, us: None }
-    }
-    pub async fn get_url(self, keyword: &str) -> std::result::Result<Self, reqwest::Error> {
-        let url = format!(
-            "https://dictionary.cambridge.org/dictionary/english/{}",
-            keyword
-        );
-        let response = reqwest::get(url).await?.text().await?;
+  pub fn new() -> Self {
+    Self { uk: None, us: None }
+  }
+  pub async fn get_url(self, keyword: &str) -> std::result::Result<Self, reqwest::Error> {
+    let url = format!(
+      "https://dictionary.cambridge.org/dictionary/english/{}",
+      keyword
+    );
+    let response = reqwest::get(url).await?.text().await?;
 
-        let uk_path = Regex::new(r#"media/english/uk_pron/[\S]*.mp3"#)
-            .unwrap()
-            .find(&response)
-            .map(|x| x.as_str());
-        let us_path = Regex::new(r#"media/english/us_pron/[\S]*.mp3"#)
-            .unwrap()
-            .find(&response)
-            .map(|x| x.as_str());
+    let uk_path = Regex::new(r#"media/english/uk_pron[\S]*.ogg"#)
+      .unwrap()
+      .find(&response)
+      .map(|x| x.as_str());
+    let us_path = Regex::new(r#"media/english/us_pron[\S]*.ogg"#)
+      .unwrap()
+      .find(&response)
+      .map(|x| x.as_str());
 
-        let host = String::from("https://dictionary.cambridge.org/");
+    let host = String::from("https://dictionary.cambridge.org/");
 
-        Ok(Self {
-            uk: if uk_path == None {
-                None
-            } else {
-                Some(host.to_owned() + uk_path.unwrap())
-            },
-            us: if uk_path == None {
-                None
-            } else {
-                Some(host.to_owned() + us_path.unwrap())
-            },
-        })
-    }
+    Ok(Self {
+      uk: if uk_path == None {
+        None
+      } else {
+        Some(host.to_owned() + uk_path.unwrap())
+      },
+      us: if uk_path == None {
+        None
+      } else {
+        Some(host.to_owned() + us_path.unwrap())
+      },
+    })
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,129 +4,134 @@ use rodio::{source::Source, Decoder, OutputStream};
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 use std::env;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
 use std::io::Cursor;
 use std::result::Result;
+use tempfile::Builder;
 use tokio;
 use wd_dict::{lookup, LookupResult, Voice};
 
-const VERSION: &str = "0.3.0";
+const VERSION: &str = "0.3.1";
 
 async fn process(to_search: &str) -> std::result::Result<LookupResult, reqwest::Error> {
-    let result = lookup(&to_search).await;
-    match result {
-        Ok(ref result) => {
-            let mut pronunciation = String::new();
-            if let Some(ref uk_pronunciation) = result.uk_pronunciation {
-                pronunciation.push_str(&format!("英 {}  ", &uk_pronunciation));
-            }
-            if let Some(ref us_pronunciation) = result.us_pronunciation {
-                pronunciation.push_str(&format!("美 {}", &us_pronunciation));
-            }
-            if pronunciation.len() > 0 {
-                println!("{}", pronunciation.cyan());
-            }
-            if let Some(ref definition) = result.definition {
-                println!("{}\n", definition);
-            }
+  let result = lookup(&to_search).await;
+  match result {
+    Ok(ref result) => {
+      let mut pronunciation = String::new();
+      if let Some(ref uk_pronunciation) = result.uk_pronunciation {
+        pronunciation.push_str(&format!("英 {}  ", &uk_pronunciation));
+      }
+      if let Some(ref us_pronunciation) = result.us_pronunciation {
+        pronunciation.push_str(&format!("美 {}", &us_pronunciation));
+      }
+      if pronunciation.len() > 0 {
+        println!("{}", pronunciation.cyan());
+      }
+      if let Some(ref definition) = result.definition {
+        println!("{}\n", definition);
+      }
 
-            if let Some(ref suggestions) = result.suggestions {
-                println!("Are you looking for");
-                for item in suggestions {
-                    println!("{}: {}", item.word.magenta(), item.definition);
-                }
-                println!();
-            }
+      if let Some(ref suggestions) = result.suggestions {
+        println!("Are you looking for");
+        for item in suggestions {
+          println!("{}: {}", item.word.magenta(), item.definition);
+        }
+        println!();
+      }
 
-            if result.suggestions == None
-                && result.definition == None
-                && result.uk_pronunciation == None
-                && result.us_pronunciation == None
-            {
-                println!("{}\n", "No result".red());
-            }
-        }
-        Err(ref error) => {
-            println!("{}\n", error);
-        }
+      if result.suggestions == None
+        && result.definition == None
+        && result.uk_pronunciation == None
+        && result.us_pronunciation == None
+      {
+        println!("{}\n", "No result".red());
+      }
     }
-    result
+    Err(ref error) => {
+      println!("{}\n", error);
+    }
+  }
+  result
 }
 
 async fn play_pronunciation(url: &str) -> Result<(), reqwest::Error> {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let response = reqwest::get(url).await?;
-    let cursor = Cursor::new(response.bytes().await.unwrap()); // Adds Read and Seek to the bytes via Cursor
-    let source = Decoder::new(cursor).unwrap(); // Decoder requires it's source to impl both Read and Seek
-    let _play = stream_handle.play_raw(source.convert_samples());
-    std::thread::sleep(std::time::Duration::from_millis(1500));
-    Ok(())
+  // Reference: https://stackoverflow.com/questions/63463503/playing-audio-from-url-in-rust
+  let (_stream, stream_handle) = OutputStream::try_default().expect("Initialize error");
+  let response = reqwest::get(url).await?;
+  let cursor = Cursor::new(response.bytes().await.expect("Get bytes error"));
+  let source = Decoder::new(cursor).expect("Decode error");
+  let _play = stream_handle.play_raw(source.convert_samples());
+  std::thread::sleep(std::time::Duration::from_millis(1500));
+  Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    let mut args: Vec<String> = env::args().collect();
+  let mut args: Vec<String> = env::args().collect();
 
-    if args.contains(&String::from("-h")) | args.contains(&String::from("--help")) {
-        print_help();
-    } else if args.contains(&String::from("-v")) | args.contains(&String::from("--version")) {
-        println!("{}", VERSION);
-    } else if args.len() > 1 {
-        args.remove(0);
-        let to_search = args.join(" ");
-        let _result = process(&to_search).await;
-    } else {
-        let mut rl = Editor::<()>::new();
-        let mut voice = Voice::new();
-        let mut is_first_search = true;
-        loop {
-            let readline = rl.readline("~ ");
-            match readline {
-                Ok(input) => {
-                    if input.is_empty() {
-                        continue;
-                    } else if input == "1" || input == "2" {
-                        if is_first_search {
-                            println!("Search something first and then send '1' or '2' to play pronunciation\n");
-                        } else if voice.uk != None && input == "1" {
-                            play_pronunciation(&voice.uk.as_ref().unwrap())
-                                .await
-                                .unwrap_or_else(|e| println!("{}", e));
-                        } else if voice.us != None && input == "2" {
-                            play_pronunciation(&voice.us.as_ref().unwrap())
-                                .await
-                                .unwrap_or_else(|e| println!("{}", e));
-                        } else {
-                            println!(
-                                "{}",
-                                "No pronunciation playback found for this word\n".red()
-                            );
-                        }
-                    } else {
-                        rl.add_history_entry(input.as_str());
-                        is_first_search = false;
-                        let result = process(&input).await;
-                        if let Ok(_) = result {
-                            let voice_result = Voice::new().get_url(&input).await;
-                            if let Ok(voice_result) = voice_result {
-                                voice = voice_result;
-                            }
-                        }
-                    }
-                }
-                Err(ReadlineError::Interrupted) => {
-                    break;
-                }
-                Err(error) => {
-                    println!("Error: {:?}\n", error);
-                }
+  if args.contains(&String::from("-h")) | args.contains(&String::from("--help")) {
+    print_help();
+  } else if args.contains(&String::from("-v")) | args.contains(&String::from("--version")) {
+    println!("{}", VERSION);
+  } else if args.len() > 1 {
+    args.remove(0);
+    let to_search = args.join(" ");
+    let _result = process(&to_search).await;
+  } else {
+    let mut rl = Editor::<()>::new();
+    let mut voice = Voice::new();
+    let mut is_first_search = true;
+    loop {
+      let readline = rl.readline("~ ");
+      match readline {
+        Ok(input) => {
+          if input.is_empty() {
+            continue;
+          } else if input == "1" || input == "2" {
+            if is_first_search {
+              println!("Search something first and then send '1' or '2' to play pronunciation\n");
+            } else if voice.uk != None && input == "1" {
+              play_pronunciation(&voice.uk.as_ref().unwrap())
+                .await
+                .unwrap_or_else(|e| println!("{}", e));
+            } else if voice.us != None && input == "2" {
+              play_pronunciation(&voice.us.as_ref().unwrap())
+                .await
+                .unwrap_or_else(|e| println!("{}", e));
+            } else {
+              println!(
+                "{}",
+                "No pronunciation playback found for this word\n".red()
+              );
             }
+          } else {
+            rl.add_history_entry(input.as_str());
+            is_first_search = false;
+            let result = process(&input).await;
+            if let Ok(_) = result {
+              let voice_result = Voice::new().get_url(&input).await;
+              if let Ok(voice_result) = voice_result {
+                voice = voice_result;
+              }
+            }
+          }
         }
+        Err(ReadlineError::Interrupted) => {
+          break;
+        }
+        Err(error) => {
+          println!("Error: {:?}\n", error);
+        }
+      }
     }
+  }
 }
 
 fn print_help() {
-    println!(
-        "CLI 有道词典 (v{})
+  println!(
+    "CLI 有道词典 (v{})
 
 Interactive mode: 
 wd
@@ -140,6 +145,6 @@ Flags:
 -h --help     Show help message
 -v --version  Show version
     ",
-        VERSION
-    );
+    VERSION
+  );
 }
